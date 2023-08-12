137ac5e6efc24fe2afd5eb3bdbd4312da8f4598e
imsk17 <imsumitkr17@gmail.com>
2023-08-13 01:57:49 +0530

    Display: Import SDL2 For Graphics

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,3 +1,73 @@
+use std::{env, fs::File, io::Read};
+
+use chip8_core::{Emu, SCREEN_HEIGHT, SCREEN_WIDTH};
+use sdl2::{event::Event, pixels::Color, rect::Rect, render::Canvas, video::Window};
+
+const SCALE: u32 = 15;
+const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
+const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: cargo run path/to/game");
+        return;
+    }
+
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Chip 8 Emulator by imsk17", WINDOW_WIDTH, WINDOW_HEIGHT)
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    canvas.clear();
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    let mut chip8 = Emu::new();
+
+    let mut rom = File::open(&args[1]).expect("Unable to open file");
+    let mut buffer = Vec::new();
+
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
+
+    'gameloop: loop {
+        for evt in event_pump.poll_iter() {
+            match evt {
+                Event::Quit { .. } => {
+                    break 'gameloop;
+                }
+                _ => (),
+            }
+            chip8.tick();
+            draw_screen(&chip8, &mut canvas)
+        }
+    }
+}
+
+fn draw_screen(emu: &Emu, canvas: &mut Canvas<Window>) {
+    // Clear canvas as black
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+    let screen_buf = emu.get_display();
+    // Now set draw color to white, iterate through each point and see if it should be drawn
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+    for (i, pixel) in screen_buf.iter().enumerate() {
+        if *pixel {
+            // Convert our 1D array's index into a 2D (x,y) position
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+            // Draw a rectangle at (x,y), scaled up by our SCALE value
+
+            let rect = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(rect).unwrap();
+        }
+    }
+    canvas.present();
 }
